e97602b814fbaea7bed1c06a1e10bd7dad7b73db
Alvils Sture <alvils.sture@gmail.com>
2021-11-01 15:10:24 +0200

    added dataset classes for autoencoder

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # 02456-deep-learning-image-denoising
 02456-deep-learning-image-denoising
+
+```
+cd ..
+mkdir data
+cd data
+wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+tar -xvzf cifar-10-python.tar.gz
+rm cifar-10-python.tar.gz
+
+ 
+ 
+
+```
---
+++ dataset.py
@@ -0,0 +1,59 @@
+from torch.utils.data import Dataset
+from PIL import Image
+import numpy as np
+import os
+import cv2
+
+class AutoEncoderDataset(Dataset):    
+    def __init__(self, img_path, transform = None):
+        """  Full court dataset class for training smp models
+        Args:
+            img_path (str): path to the image folder
+            transform (albumentations.Compose): augmentation transforms to apply to images
+        """
+
+        self.transform = transform
+        self.img_file_path = []
+
+        # stores full path of images
+        self.img_files = os.listdir(img_path);
+        for file in  self.img_files:
+            self.img_file_path.append(os.path.join(img_path, file))
+            
+    def __len__(self):
+        return len(self.img_file_path)
+
+    def __getitem__(self, idx):
+        img_in = cv2.imread(self.img_file_path[idx], cv2.COLOR_BGR2RGB)
+        img_out = img_in.copy()
+        # applies transformations
+        if self.transform:
+            sample = self.transform(image=img_out)
+            img_out = sample['image']
+            
+        return img_out, img_in
+    
+    
+class Cifar10AutoEncoderDataset(Dataset):    
+    def __init__(self, imgs, transform = None):
+        """  Full court dataset class for training smp models
+        Args:
+            img_path (str): path to the image folder
+            transform (albumentations.Compose): augmentation transforms to apply to images
+        """
+
+        self.transform = transform
+        self.imgs = imgs
+            
+    def __len__(self):
+        return len(self.imgs)
+
+    def __getitem__(self, idx):
+        img_in = self.imgs[idx]
+        img_out = img_in.copy()
+        # applies transformations
+        if self.transform:
+            sample = self.transform(image=img_out)
+            img_out = sample['image']
+            
+        return img_out, img_in
